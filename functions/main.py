57f94a22702165ad0f36d8e52ec7baec8d27e773
Thomas Tumiel <ttumiel@gmail.com>
2023-10-20 01:44:25 +0100

    finalize firebase

--- functions/main.py
+++ functions/main.py
@@ -4,7 +4,7 @@ from typing import List, Union
 
 from chat2func import function_call
 from firebase_admin import initialize_app
-from firebase_functions import https_fn, options
+from firebase_functions import https_fn, options, scheduler_fn
 from xplorer.functions import ArxivXplorerAPI, SearchMethod
 
 initialize_app()
@@ -66,3 +66,22 @@ def search(
     method: SearchMethod = SearchMethod.SEMANTIC,
 ):
     return api.search(query, count, page, year, method)
+
+
+@scheduler_fn.on_schedule(
+    schedule="every week 05:00",
+    timezone="UTC",
+    region=options.SupportedRegion.US_WEST1,
+    memory=options.MemoryOption.MB_256,
+    max_instances=1,
+    cpu=1,
+)
+def db_clean(event: scheduler_fn.ScheduledEvent):
+    "Delete the least recently used papers from firestore"
+    try:
+        num_deleted = api.cache.remote_db.lru_delete()
+    except Exception as e:
+        print(f"ERROR :: Function db_clean failed:\n", traceback.format_exc())
+        raise e
+
+    print("Deleted", num_deleted, "papers from firestore")
